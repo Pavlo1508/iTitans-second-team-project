2435fa7afd5708b70717ea69a25bb4d58c5907c9
Yana Lepska <lepskayana1624@gmail.com>
2024-02-25 00:11:25 +0200

    Update code

--- src/js/side-bar/api.js
+++ src/js/side-bar/api.js
@@ -4,7 +4,7 @@ export async function getCategories() {
 	try {
 		axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
 		const response = await axios.get('/category-list');
-		return response.data.map(item => item.list_name);
+		return response.data.map(item => item.list_name).toSorted();
 	} catch (error) {
 		return console.error(error);
 	};
--- src/js/side-bar/render-sb.js
+++ src/js/side-bar/render-sb.js
@@ -1,29 +1,61 @@
 const categoriesList = document.getElementById('categories-list');
-let previousItem = null;
+let previousItem = '';
 export function renderSb(categories) {
+
     categories.forEach(category => {
-			const categoryItem = document.createElement('li');
-			// створюємо лішку з кожного елементу масиву
-			categoryItem.classList.add('category-item');
-			// додаєм до лішки клас
-			categoryItem.textContent = category;
-			// додаєм до лішки текст який буде з'являтись
+		const categoryItem = document.createElement('li');
+		// створюємо лішку з кожного елементу масиву
+		categoryItem.classList.add('category-item');
+		categoryItem.classList.add('dark-category-item');
+		// додаєм до лішки клас
+		categoryItem.textContent = category;
+		// додаєм до лішки текст який буде з'являтись
+		categoriesList.appendChild(categoryItem);
+		// рендерим створену лішку
+	});
 
-        categoryItem.addEventListener('click', () => {
-					categoryItem.textContent = categoryItem.textContent.toUpperCase();
-					// при кліку переводимо текст до верхнього регістру
-
-            if (previousItem && previousItem !== categoryItem) {
-                previousItem.textContent = previousItem.getAttribute('data-text');
-					}
-					// перевіряємо, чи є попередній елемент та чи він не є вибраним елементом. якщо так, то змінюємо текст попереднього елемента на текст який був спочатку.
-					previousItem = categoryItem;
-					// зберігаєм текст вибраного єлемента для наступних прівнянь
+	const allCategoryItems = document.querySelectorAll('.category-item');
+	
+	allCategoryItems.forEach(allCategoryItems => {
+	allCategoryItems.addEventListener('click', () => {
+		addStyle(allCategoryItems);
+		allCategoryItems.textContent = allCategoryItems.textContent.toUpperCase();
+		// при кліку переводимо текст до верхнього регістру
+        if (previousItem && previousItem !== allCategoryItems) {
+			previousItem.textContent = previousItem.getAttribute('data-text');
+			resetStyle(previousItem);
+		}
+		// перевіряємо, чи є попередній елемент та чи він не є вибраним елементом
+		// якщо так, то змінюємо текст попереднього елемента на текст який був спочатку.
+		previousItem = allCategoryItems;
+		// зберігаєм текст вибраного єлемента для наступних прівнянь
         });
 
-        categoryItem.setAttribute('data-text', categoryItem.textContent);
-				// додаєм артибут у якому зберігаєм текст
-			categoriesList.appendChild(categoryItem);
-			// рендерим створену лішку
-    });
+        allCategoryItems.setAttribute('data-text', allCategoryItems.textContent);
+		// додаєм артибут у якому зберігаєм текст
+	});	
+}
+
+function addStyle(allCategoryItems) {
+	const bodyStyle = window.getComputedStyle(document.body);
+    const backgroundColor = bodyStyle.backgroundColor;
+	if (backgroundColor === 'rgb(32, 32, 36)') {
+	allCategoryItems.style.color = '#EAC645';
+    allCategoryItems.style.fontWeight = '700';
+	} else {
+	allCategoryItems.style.color = '#4F2EE8';
+    allCategoryItems.style.fontWeight = '700';	
+	}
+	
+}
+function resetStyle(previousItem) {
+  	const bodyStyle = window.getComputedStyle(document.body);
+	const backgroundColor = bodyStyle.backgroundColor;
+	if (backgroundColor === 'rgb(32, 32, 36)') {
+	previousItem.style.color = 'rgb(255, 255, 255, 0.6)';
+    previousItem.style.fontWeight = '400';
+	} else {
+	previousItem.style.color = 'rgb(17, 17, 17, 0.6)';
+    previousItem.style.fontWeight = '400';
+	}
 }
