4a43bc9a35f2ecb593a8cf57bccf1b98190caa80
Yana Lepska <lepskayana1624@gmail.com>
2024-02-25 19:52:01 +0200

    ButtonToUp

--- src/js/side-bar/sidebar.js
+++ src/js/side-bar/sidebar.js
@@ -1,8 +1,19 @@
 import { getCategories } from './api';
 import { renderSb } from './render-sb';
+import { scrollToUp } from './ToUpBtn';
 
 
 export async function initializeSb() {
     const categories = await getCategories();
     renderSb(categories);
+<<<<<<< Updated upstream
 };
+=======
+};
+initializeSb();
+scrollToUp();
+// ПЕРЕНЕСТИ В МЕЙН
+// import { initializeSb } from './side-bar/sidebar'
+// initializeSb();
+
+>>>>>>> Stashed changes
