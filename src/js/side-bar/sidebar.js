0445da28659425ecc8e2742822144b92a6be5d37
Pavlo1508 <pasha150898@gmail.com>
2024-02-25 01:22:27 +0100

    updape main

--- src/js/side-bar/sidebar.js
+++ src/js/side-bar/sidebar.js
@@ -6,7 +6,7 @@ export async function initializeSb() {
     const categories = await getCategories();
     renderSb(categories);
 };
-
+initializeSb();
 // ПЕРЕНЕСТИ В МЕЙН
 // import { initializeSb } from './side-bar/sidebar'
 // initializeSb();
