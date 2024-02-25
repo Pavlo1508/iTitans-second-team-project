f55ca9c2cc53c55333f050e11c91eb0178ae6ff5
Pavlo1508 <pasha150898@gmail.com>
2024-02-25 20:28:03 +0100

    .

--- src/js/hero/hero.js
+++ src/js/hero/hero.js
@@ -79,38 +79,5 @@ window.addEventListener('resize', () => {
   document.querySelector('.hero-list').innerHTML = '';
   renderBookCards();
 });
-
-
-const loadBtn = document.querySelector('.button-load');
-
-loadBtn.addEventListener('click', async () => {
-    const data = await getImg();
-    render(data);
-    lightbox.refresh();
-    const galleryCards = document.querySelectorAll('.gallery-card');
-});
-
-function render(imgs) {
-    const markup = imgs.map((img) => {
-        return `<li class="gallery-card">
-    <a class="" href="$">
-        <img src="" alt="" />
-    </ul>
-    </a>
-</li>`;
-    }).join('');
-
-    list.insertAdjacentHTML('beforeend', markup);
-}
-
-
-async function getImg() {
-    
-    const params = new URLSearchParams({
-    });
-    
-    const response = await axios.get(`https://books-backend.p.goit.global/books/top-books&_limit=5&_sort=name`);
-    return response.data;
-}
 // ПЕРЕНЕСТИ В MAIN.JS
 // import './hero';
