4d0c3508ea049cc901d165350b61417b46deb5da
Satanw8you <trusnikolaj41@gmail.com>
2024-02-25 22:07:17 +0200

    Update dark.js

--- src/js/dark.js
+++ src/js/dark.js
@@ -37,14 +37,14 @@ const fondsCont = document.querySelector('.supporters');
 openBtn.addEventListener('click', () => {
   if (menuCont.classList.contains('none')) {
     menuCont.classList.remove('none');
-    fondsCont.style.display = 'none';
+    fondsCont.classList.add('visually-hidden');
     openBtn.innerHTML = `<svg class="icon-menu-close" width="14" height="14">
         <use href="./img/fonds/svg/icons.svg#icon-burg-close"></use>
       </svg>`;
     document.body.classList.add('mobile-menu-open');
   } else {
     menuCont.classList.add('none');
-    fondsCont.style.display = 'block';
+    fondsCont.classList.remove('visually-hidden');
     openBtn.innerHTML = `<svg class="icon-menu" width="28" height="28">
         <use href="./img/fonds/svg/icons.svg#icon-burger-menu"></use>
       </svg>`;
