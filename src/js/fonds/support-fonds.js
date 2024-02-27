fe5def9f761c08ea30e4a9d2ca244cc02e1786b4
S1a2s <konov.a.9911@gmail.com>
2024-02-27 14:44:53 +0200

    import fonds img

--- src/js/fonds/support-fonds.js
+++ src/js/fonds/support-fonds.js
@@ -1,65 +1,92 @@
+import save from '/src/img/fonds/png/save-the-children@1x.png';
+import save2 from '/src/img/fonds/png/save-the-children@2x.png';
+import save3 from '/src/img/fonds/png/save-the-children@3x.png';
+import hope from '/src/img/fonds/png/project-hope@1x.png';
+import hope2 from '/src/img/fonds/png/project-hope@2x.png';
+import hope3 from '/src/img/fonds/png/project-hope@3x.png';
+import inter from '/src/img/fonds/png/international-medical-corps@1x.png';
+import inter2 from '/src/img/fonds/png/international-medical-corps@2x.png';
+import inter3 from '/src/img/fonds/png/international-medical-corps@3x.png';
+import razom from '/src/img/fonds/png/razom@1x.png';
+import razom2 from '/src/img/fonds/png/razom@2x.png';
+import razom3 from '/src/img/fonds/png/razom@3x.png';
+import action from '/src/img/fonds/png/action-against-hunger@1x.png';
+import action2 from '/src/img/fonds/png/action-against-hunger@2x.png';
+import action3 from '/src/img/fonds/png/action-against-hunger@3x.png';
+import serhiy from '/src/img/fonds/png/serhiy-prytula-charity-foundation@1x.png';
+import serhiy2 from '/src/img/fonds/png/serhiy-prytula-charity-foundation@2x.png';
+import serhiy3 from '/src/img/fonds/png/serhiy-prytula-charity-foundation@3x.png';
+import medicins from '/src/img/fonds/png/medicins-sans-frontieres@1x.png';
+import medicins2 from '/src/img/fonds/png/medicins-sans-frontieres@2x.png';
+import medicins3 from '/src/img/fonds/png/medicins-sans-frontieres@3x.png';
+import world from '/src/img/fonds/png/world-vision@1x.png';
+import world2 from '/src/img/fonds/png/world-vision@2x.png';
+import world3 from '/src/img/fonds/png/world-vision@3x.png';
+import united from '/src/img/fonds/png/united24@1x.png';
+import united2 from '/src/img/fonds/png/united24@2x.png';
+import united3 from '/src/img/fonds/png/united24@3x.png';
 export const supporters = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './img/fonds/png/save-the-children@1x.png',
-    imgRetinaTwo: './img/fonds/png/save-the-children@2x.png',
-    imgRetinaThree: './img/fonds/png/save-the-children@3x.png',
+    img: save,
+    imgRetinaTwo: save2,
+    imgRetinaThree: save3,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: './img/fonds/png/project-hope@1x.png',
-    imgRetinaTwo: './img/fonds/png/project-hope@2x.png',
-    imgRetinaThree: './img/fonds/png/project-hope@3x.png',
+    img: hope,
+    imgRetinaTwo: hope2,
+    imgRetinaThree: hope3,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: './img/fonds/png/international-medical-corps@1x.png',
-    imgRetinaTwo: './img/fonds/png/international-medical-corps@2x.png',
-    imgRetinaThree: './img/fonds/png/international-medical-corps@3x.png',
+    img: inter,
+    imgRetinaTwo: inter2,
+    imgRetinaThree: inter3,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: './img/fonds/png/razom@1x.png',
-    imgRetinaTwo: './img/fonds/png/razom@2x.png',
-    imgRetinaThree: './img/fonds/png/razom@3x.png',
+    img: razom,
+    imgRetinaTwo: razom2,
+    imgRetinaThree: razom3,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: './img/fonds/png/action-against-hunger@1x.png',
-    imgRetinaTwo: './img/fonds/png/action-against-hunger@2x.png',
-    imgRetinaThree: './img/fonds/png/action-against-hunger@3x.png',
+    img: action,
+    imgRetinaTwo: action2,
+    imgRetinaThree: action3,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: './img/fonds/png/serhiy-prytula-charity-foundation@1x.png',
-    imgRetinaTwo: './img/fonds/png/serhiy-prytula-charity-foundation@2x.png',
-    imgRetinaThree: './img/fonds/png/serhiy-prytula-charity-foundation@3x.png',
+    img: serhiy,
+    imgRetinaTwo: serhiy2,
+    imgRetinaThree: serhiy3,
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: './img/fonds/png/medicins-sans-frontieres@1x.png',
-    imgRetinaTwo: './img/fonds/png/medicins-sans-frontieres@2x.png',
-    imgRetinaThree: './img/fonds/png/medicins-sans-frontieres@3x.png',
+    img: medicins,
+    imgRetinaTwo: medicins2,
+    imgRetinaThree: medicins3,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: './img/fonds/png/world-vision@1x.png',
-    imgRetinaTwo: './img/fonds/png/world-vision@2x.png',
-    imgRetinaThree: './img/fonds/png/world-vision@3x.png',
+    img: world,
+    imgRetinaTwo: world2,
+    imgRetinaThree: world3,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: './img/fonds/png/united24@1x.png',
-    imgRetinaTwo: './img/fonds/png/united24@2x.png',
-    imgRetinaThree: './img/fonds/png/united24@3x.png',
+    img: united,
+    imgRetinaTwo: united2,
+    imgRetinaThree: united3,
   },
 ];
