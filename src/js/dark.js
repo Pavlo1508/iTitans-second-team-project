29d0267dc5b3f451e109135be0be85d62122f584
Pavlo1508 <pasha150898@gmail.com>
2024-02-27 22:40:15 +0100

    dark

--- src/js/dark.js
+++ src/js/dark.js
@@ -5,50 +5,49 @@ const shoppingList = document.querySelector('.nav-shopping');
 const shoppingIcon = document.querySelector('.shopping-icon');
 const body = document.body;
 const header = document.querySelector('header');
-const closeIcon = document.querySelector('.icon-close');
+const btnOpen = document.querySelector('.icon-menu-close');
+const closeIcon = document.querySelector('.icon-menu');
 const key = 'dark-theme';
 switcher.addEventListener('click', changeTheme);
 function changeTheme() {
   if (switcher.checked) {
     shoppingIcon.classList.add('dark-icons');
     shoppingList.classList.add('dark-icons');
-    closeIcon.classList.add('dark-icons');
     logo.classList.add('dark-icons');
     menu.classList.add('dark-menu');
     body.classList.add('dark-mode-body');
     header.classList.add('dark-mode');
+    btnOpen.classList.add('dark-icon-menu');
+    closeIcon.classList.add('dark-icon-menu');
     // localStorage.setItem(key, JSON.stringify(switcher.checked))
   } else {
     shoppingIcon.classList.remove('dark-icons');
     shoppingList.classList.remove('dark-icons');
-    closeIcon.classList.remove('dark-icons');
     logo.classList.remove('dark-icons');
     menu.classList.remove('dark-menu');
     body.classList.remove('dark-mode-body');
     header.classList.remove('dark-mode');
+    btnOpen.classList.remove('dark-icon-menu');
+    closeIcon.classList.remove('dark-icon-menu');
   }
 }
-
-
 //===============================//
 const menuCont = document.querySelector('.mobile-menu-container');
 const openBtn = document.querySelector('.btn-menu');
 const fondsCont = document.querySelector('.supporters');
-
-openBtn.addEventListener('click', () => {
+openBtn.addEventListener('click', onBtnOpenClick);
+function onBtnOpenClick() {
   if (menuCont.classList.contains('none')) {
     menuCont.classList.remove('none');
     fondsCont.classList.add('visually-hidden');
-    openBtn.innerHTML = `<svg class="icon-menu-close" width="14" height="14">
-        <use href="./img/fonds/svg/icons.svg#icon-burg-close"></use>
-      </svg>`;
     document.body.classList.add('mobile-menu-open');
+    btnOpen.classList.remove('visually-hidden');
+    closeIcon.classList.add('visually-hidden');
   } else {
     menuCont.classList.add('none');
     fondsCont.classList.remove('visually-hidden');
-    openBtn.innerHTML = `<svg class="icon-menu" width="28" height="28">
-        <use href="./img/fonds/svg/icons.svg#icon-burger-menu"></use>
-      </svg>`;
     document.body.classList.remove('mobile-menu-open');
+    btnOpen.classList.add('visually-hidden');
+    closeIcon.classList.remove('visually-hidden');
   }
-});
+}
