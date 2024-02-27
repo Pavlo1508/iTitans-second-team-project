30a007ecfb0bd86fc378a47b6eedb27358bef2a0
Pavlo1508 <pasha150898@gmail.com>
2024-02-27 22:30:37 +0100

    Update shopping-list.js

--- src/js/shoping-list/shopping-list.js
+++ src/js/shoping-list/shopping-list.js
@@ -1,7 +1,7 @@
 import { pagination } from './pagination'
 import amazonIcon from '../../img/png/amazon.png';
 import appleB from '../../img/png/Apple books.png';
-import dump from 'img/png/dump.png'
+import dump from './img/png/dump.png'
 
 function hideSidebar() {
 	const tagTitle = document.querySelector('.hero-title');
