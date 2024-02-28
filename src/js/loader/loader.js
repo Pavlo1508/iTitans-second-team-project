13b4cd9d102d4586e64e12c571bf36f6f101864f
Pavlo1508 <pasha150898@gmail.com>
2024-02-28 20:07:23 +0100

    .

--- src/js/loader/loader.js
+++ src/js/loader/loader.js
@@ -1,23 +1,23 @@
 function showLoader() {
-    var loaderOverlay = document.getElementById('loader-overlay');
+    let loaderOverlay = document.getElementById('loader-overlay');
     if (loaderOverlay) {
       loaderOverlay.style.display = 'flex';
     }
   }
   
   function hideLoader() {
-    var loaderOverlay = document.getElementById('loader-overlay');
+    let loaderOverlay = document.getElementById('loader-overlay');
     if (loaderOverlay) {
       loaderOverlay.style.display = 'none';
     }
   }
   
   function interceptXHRRequests() {
-    var originalOpen = XMLHttpRequest.prototype.open;
-    var originalSend = XMLHttpRequest.prototype.send;
+    let originalOpen = XMLHttpRequest.prototype.open;
+    let originalSend = XMLHttpRequest.prototype.send;
   
     XMLHttpRequest.prototype.open = function () {
-      var self = this;
+      let self = this;
       self.addEventListener('loadstart', function () {
         showLoader();
       });
@@ -28,7 +28,7 @@ function showLoader() {
     };
   
     XMLHttpRequest.prototype.send = function () {
-      var self = this;
+      let self = this;
       self.addEventListener('loadstart', function () {
         showLoader();
       });
@@ -40,7 +40,7 @@ function showLoader() {
   }
   
   function interceptFetchRequests() {
-    var originalFetch = window.fetch;
+    let originalFetch = window.fetch;
   
     window.fetch = function () {
       showLoader();
@@ -66,7 +66,7 @@ function showLoader() {
   });
   
   function updateLoaderStyle() {
-    var loaderOverlay = document.getElementById('loader-overlay');
+    let loaderOverlay = document.getElementById('loader-overlay');
     if (document.body.classList.contains('dark')) {
       loaderOverlay.style.backgroundColor = 'rgba(17, 17, 17)';
       loaderOverlay.querySelector('.loader').style.top = '10px'; // змінюємо положення тексту
