8a44366b1a761878bffb4939e3fe57044b3ffec3
Melnyk Roman <melnikroman2005@ukr.net>
2024-02-29 17:25:31 +0200

    іконка toUpBtn

--- src/js/ToUpBtn.js
+++ src/js/ToUpBtn.js
@@ -1,30 +1,35 @@
 const btn = document.querySelector('.btn-ring');
+const arrowToUpIcon = document.querySelector('.arrow-to-up-icon');
+
+arrowToUpIcon.setAttribute(
+  'href',
+  '../img/sprite.svg#icon-material-symbols_arrow-back-ios-rounded'
+);
 
 export function scrollToUp() {
- window.addEventListener("scroll", () => {
-    if ( window.scrollY > 250 || document.documentElement.scrollTop > 250) {
-        showButton();
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 250 || document.documentElement.scrollTop > 250) {
+      showButton();
     } else {
-       hideButton() 
+      hideButton();
     }
-});
+  });
 
-btn.onclick = () => {
-     window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
-};   
+  btn.onclick = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
 }
 
-
 function showButton() {
-  btn.classList.remove('btn-ring-hidden');  
+  btn.classList.remove('btn-ring-hidden');
 }
 
 function hideButton() {
-     btn.classList.add('btn-ring-hidden'); 
+  btn.classList.add('btn-ring-hidden');
 }
 
 scrollToUp();
